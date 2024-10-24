0ce85628a827f5378ec1253207d83ee826824006
NielsKuipers1 <niels.jan.kuipers@gmail.com>
2024-10-24 12:50:05 +0200

    first commit

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import cv2
 import numpy as np
 import os
 
-def detect_lines(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    edges = cv2.Canny(blurred, 50, 150)
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=50, maxLineGap=10)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    return frame, lines
+def detect_lines(frame,boundaries):
+    intersections = []
 
-
+    # remove colors of lower values (the lines are white)
+    for (lower, upper) in boundaries:
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
+        mask = cv2.inRange(frame, lower, upper)
+        output = cv2.bitwise_and(frame, frame, mask=mask)
+    gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+    corners = cv2.cornerHarris(gray, 9, 3, 0.01) 
+    corners = cv2.normalize(corners, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    _, thresh = cv2.threshold(corners, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    dilated = cv2.dilate(thresh, None, iterations=1)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        moments = cv2.moments(contour)
+        if moments["m00"] != 0:
+            cx = int(moments["m10"] / moments["m00"])
+            cy = int(moments["m01"] / moments["m00"])
+            cv2.circle(frame, (cx, cy), 3, (0, 0, 255), -1)
+            intersections.append(contour)
+    return frame, intersections
 
 video_path = 'video.mp4'
 cap = cv2.VideoCapture(video_path)
@@ -22,13 +33,15 @@ fps = cap.get(cv2.CAP_PROP_FPS)
 frames_per_segment = int(fps * 2.5)  
 total_segments = 8
 total_frames = frames_per_segment * total_segments
+boundaries = [([170, 170, 100], [255, 255, 255])]
+
 
 while frame_count < total_frames:
     ret, frame = cap.read()
     if not ret:
         break
     # Step 1: corner detection
-    frame, lines = detect_lines(frame)
+    frame, intersections = detect_lines(frame,boundaries)
     cv2.imshow('frame',frame)
     cv2.waitKey(25)
     frame_count += 1 
